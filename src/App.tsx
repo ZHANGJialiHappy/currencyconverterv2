7995ae27efb833ad1da507a28be321af9f92629d
Jiali Zhang <zhangjiali86@gmail.com>
2023-02-21 15:21:47 +0100

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ function App() {
     .then (res => res.json())
     .then ((data) => {
       const firstCurrency = Object.keys(data.rates)[0]
-      console.log(data);
       setCurrencyOption(Object.keys(data.rates));
       setFromCurrency(data.base);
       setToCurrency(firstCurrency);
@@ -24,23 +23,61 @@ function App() {
     })
   }, [])
 
-  // if(amountInFromCurrency){
-  //   set
-  // }
+  let fromAmount: number | string, toAmount: number | string
+  if(amountInFromCurrency){
+    fromAmount = amount;
+    toAmount = typeof fromAmount === "number" && fromAmount >= 0 ? (Math.round(fromAmount * exchangeRate * 100) / 100) : ""
+  } else {
+    toAmount =amount;
+    fromAmount = typeof toAmount === "number" && toAmount >= 0 ? (Math.round(toAmount / exchangeRate * 100) / 100) : ""
+  }
+
+  const handelFromAmount = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value= e.target.value;
+    if(value){
+      setAmount(Number(value));
+      // setAmount(Number(value));
+
+    } else setAmount("")
+    setamountInFromCurrency(true);
+  }
+
+  const handelToAmount = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value= e.target.value;
+    if(value){
+      setAmount(Number(value));
+    } else setAmount("")
+    setamountInFromCurrency(false);
+  }
+
+  useEffect(()=>{
+    fetch(`${BASE_URL}?base=${fromCurrency}&symbols=${toCurrency}`)
+    .then (res => res.json())
+    .then ((data) => setExchangeRate(data.rates[toCurrency]))
+  }, [fromCurrency, toCurrency])
 
   return (
     <div className="flex justify-center text-center items-center min-h-screen">
       <div className="text-2xl">
+
         <h1 className="text-2xl mb-5">Convert</h1>
+
         <CurrencyRow 
         currencyOption = {currencyOption} 
         selectedCurrency = {fromCurrency} 
-        onChangeCurrencyOption = {(e)=>setFromCurrency(e.target.value)}/>
+        onChangeCurrencyOption = {(e)=>setFromCurrency(e.target.value)}
+        onChangeAmount = {handelFromAmount}
+        amount={fromAmount}/>
+
         <div>=</div>
+
         <CurrencyRow 
         currencyOption = {currencyOption} 
         selectedCurrency = {toCurrency}
-        onChangeCurrencyOption = {(e)=>setToCurrency(e.target.value)}/>
+        onChangeCurrencyOption = {(e)=>setToCurrency(e.target.value)}
+        onChangeAmount= {handelToAmount}
+        amount={toAmount}/>
+        
       </div>
     </div>
 
--- src/CurrencyRow.tsx
+++ src/CurrencyRow.tsx
@@ -3,16 +3,21 @@ import React from 'react'
 type Props = {
     currencyOption: string[],
     selectedCurrency: string,
-    onChangeCurrencyOption(e: React.ChangeEvent<HTMLSelectElement>): void
+    onChangeCurrencyOption(e: React.ChangeEvent<HTMLSelectElement>): void,
+    onChangeAmount(e: React.ChangeEvent<HTMLInputElement>): void,
+    amount: number | string
 }
 
-function CurrencyRow({currencyOption,selectedCurrency,onChangeCurrencyOption}: Props ) {
+function CurrencyRow({currencyOption, selectedCurrency, onChangeCurrencyOption, onChangeAmount, amount}: Props ) {
     return (
         <div className="flex gap-5">
             <input
                 type="number"
+                min="0"
                 placeholder="Type a number"
                 className="input input-bordered input-warning w-full max-w-xs flex-none"
+                onChange={onChangeAmount}
+                value={amount}
             />
 
             <select 
